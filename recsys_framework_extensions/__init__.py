4b335775db80a835b13cfd917a31a09f61426a7e
Fernando Benjamin PEREZ MAURERA <fperezmaurera@gmail.com>
2023-11-03 21:52:43 +0100

    Changed configuration of logger for the package

--- recsys_framework_extensions/__init__.py
+++ recsys_framework_extensions/__init__.py
@@ -5,22 +5,19 @@ import toml
 
 
 with open(os.path.join(os.getcwd(), "pyproject.toml"), "r") as project_file:
-    pyproject_logs_config = toml.load(
-        f=project_file
-    )["logs"]
+    pyproject_logs_config = toml.load(f=project_file)["logs"]
 
 _dir_logger = os.path.join(
     os.getcwd(),
-    pyproject_logs_config['dir_logs'],
+    pyproject_logs_config["dir_logs"],
     "",
 )
 _filename_logger = os.path.join(
     _dir_logger,
-    pyproject_logs_config['filename_logs'],
+    pyproject_logs_config["filename_logs"],
 )
 
 os.makedirs(_dir_logger, exist_ok=True)
-
 
 # Definition of config dict seen at:
 # https://docs.python.org/3.9/library/logging.config.html#dictionary-schema-details
@@ -30,8 +27,8 @@ conf = {
     "formatters": {
         "main_formatter": {
             "format": "%(process)d|%(asctime)s|%(levelname)s"
-                      "|%(name)s|%(module)s|%(filename)s|%(funcName)s|%(lineno)d"
-                      "|%(message)s",
+            "|%(name)s|%(module)s|%(filename)s|%(funcName)s|%(lineno)d"
+            "|%(message)s",
             "validate": True,
         }
     },
@@ -56,27 +53,19 @@ conf = {
             "formatter": "main_formatter",
             "stream": sys.stderr,
             "level": logging.WARNING,
-        }
+        },
     },
     "loggers": {
         "recsys_framework_extensions": {
-            "level": logging.DEBUG,
+            "level": logging.WARNING,
             "propagate": False,
             "handlers": [
                 "file",
                 "console_out",
                 "console_error",
-            ]
+            ],
         },
     },
-    "root": {
-        "level": logging.WARNING,
-        "handlers": [
-            "file",
-            "console_out",
-            "console_error",
-        ],
-    }
 }
 
 logging.config.dictConfig(conf)
